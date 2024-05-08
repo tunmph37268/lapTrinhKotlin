f09691805724cc652352abe00483d9bc643068d7
tunmph37268 <tunmph37268@fpt.edu.vn>
2024-05-08 21:17:22 +0700

    bai4lab1

--- app/src/main/java/com/example/lab1_kotlin/Bai4Lab1.kt
+++ app/src/main/java/com/example/lab1_kotlin/Bai4Lab1.kt
@@ -27,20 +27,63 @@ fun main( args:Array<String>) {
     println("Exame5: ")
     val zoo = Zoo(listOf(Animal("zebra"), Animal("lion")))
 
-    for (animal in zoo) {                                   // 3
+    for (animal in zoo) {
         println("Watch out, it's a ${animal.name}")
     }
+    println("Exame6: ")
+    for(i in 0..3) {
+        print(i)
+    }
+    print(" ")
 
+    for(i in 0 until 3) {
+        print(i)
+    }
+    print(" ")
+
+    for(i in 2..8 step 2) {
+        print(i)
+    }
+    print(" ")
+
+    for (i in 3 downTo 0) {
+        print(i)
+    }
+    print(" ")
+    println("Exame7: ")
+    val authors = setOf("Shakespeare", "Hemingway", "Twain")
+    val writers = setOf("Twain", "Shakespeare", "Hemingway")
+
+    println(authors == writers)   // so sánh cấu trúc
+    println(authors === writers)  // so sánh tham chiếu
+    println("Exame8: ")
+    fun max(a: Int, b: Int) = if (a > b) a else b
+
+    println("GTLN:"+max( 99, -42))
+    println("Exame9: ")
+    fun min(a: Int, b: Int) = if (a < b) a else b
+
+    println("GTNN:"+min( 99, -42))
+    println("Exame10: ")
+    for (c in 'a'..'d') {
+        print(c)
+    }
+    print(" ")
+
+    for (c in 'z' downTo 's' step 2) {
+        print(c)
+    }
+    print(" ")
 }
 fun eatACake() = println("Tú No love")
 fun bakeACake() = println("Tú có Ny")
 fun cases(obj: Any) {
-    when (obj) {                                     // 1
-        1 -> println("Tú")                          // 2
-        "Hello" -> println("GoodNight")               // 3
-        is Long -> println("Long")                   // 4
-        !is String -> println("Not a string")        // 5
-        else -> println("Không tồn tại")                   // 6
+    when (obj) {
+        1 -> println("Tú")
+        "Hello" -> println("GoodNight")
+        is Long -> println("Long")
+        !is String -> println("Not a string")
+        else -> println("Không tồn tại")
     }
 }
 
