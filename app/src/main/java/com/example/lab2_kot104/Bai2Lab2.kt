c3312921fcddb6466be74e9a1d7ea5ac2ffcc85f
tunmph37268 <tunmph37268@fpt.edu.vn>
2024-05-09 11:03:27 +0700

    Lab2_KOT104

---
+++ app/src/main/java/com/example/lab2_kot104/Bai2Lab2.kt
@@ -0,0 +1,16 @@
+package com.example.lab2_kot104
+fun main() {
+    var month = 0
+    println("Nhập tháng:")
+    val s: String? = readLine()
+    if (s != null) month = s.toInt()
+    when (month) {
+        1, 2, 3 -> println("Tháng " + month + " thuộc quý 1")
+        4, 5, 6 -> println("Tháng " + month + " thuộc quý 2")
+        7, 8, 9 -> println("Tháng " + month + " thuộc quý 3")
+        10, 11, 12 -> println("Tháng " + month + " thuộc quý 4")
+        else -> println("Tháng " + month + " không hợp lệ")
+    }
+}
+class Bai2Lab2 {
+}
---
+++ app/src/main/java/com/example/lab2_kot104/Bai3Lab3.kt
@@ -0,0 +1,28 @@
+package com.example.lab2_kot104
+fun main() {
+    var year = 0
+    var s: String?
+    println("Chương trình kiểm tra năm nhuần:")
+    do {
+        println("Nhập 1 năm:")
+        s = readLine()
+        while (s == null || s.toInt() < 0) {
+            println("Nhập sai năm, nhập lại:")
+            s = readLine()
+        }
+        year = s.toInt()
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            println("Năm $year là năm nhuần")
+        } else {
+            println("Năm $year không phải là năm nhuần")
+        }
+        print("Tiếp không?(c/k):")
+        s = readLine()
+        if (s == "k")
+            break;
+    } while (true)
+    println("Kết thúc chương trình")
+}
+
+class Bai3Lab3 {
+}
