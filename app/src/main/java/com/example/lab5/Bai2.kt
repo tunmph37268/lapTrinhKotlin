91f610417086f0ab832e9a71b997e4275fff3c89
tunmph37268 <tunmph37268@fpt.edu.vn>
2024-05-29 13:26:59 +0700

    bai5

---
+++ app/src/main/java/com/example/lab5/Bai2.kt
@@ -0,0 +1,69 @@
+package com.example.lab5
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+
+class Bai2 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LightSwitch()
+        }
+    }
+}
+
+@Composable
+fun LightSwitch() {
+    Column(
+        modifier = Modifier.fillMaxSize().background(Color.Black),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        val isChecked = remember { mutableStateOf(false) }
+        if (isChecked.value) {
+            Image(
+                painter = painterResource(id = R.drawable.ligton),
+                contentDescription = "Light is On",
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.FillWidth
+            )
+        } else {
+            Image(
+                painter = painterResource(id =
+                R.drawable.ligtoff),
+                contentDescription = "Light is Off",
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.FillWidth
+            )
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+        Switch(
+            checked = isChecked.value,
+            onCheckedChange = { isChecked.value = it },
+            colors = SwitchDefaults.colors(
+                checkedThumbColor = Color.Green,
+                uncheckedThumbColor = Color.Gray,
+                checkedTrackColor = Color.Green.copy(alpha = 0.5f),
+                uncheckedTrackColor = Color.Gray.copy(alpha =
+                0.5f),
+                checkedBorderColor = Color.Green.copy(alpha =
+                0.75f),
+            )
+        )
+    }
+}
